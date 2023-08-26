ba71b1120ec278e8dc819aa1d8747f9c2f41c4c2
js3322 <js@cleancode.kr>
2023-08-26 16:51:06 +0900

    add : todo example

---
+++ pull_cluster_data/src/main/kotlin/com/cleancode/pull_cluster_data/controller/category_100/route_user.kt
@@ -0,0 +1,5 @@
+package com.cleancode.pull_cluster_data.controller.category_100
+
+// TODO [js] 개인 fix
+class route_user {
+}
--- pull_cluster_data/src/main/kotlin/com/cleancode/pull_cluster_data/controller/route.kt
+++ pull_cluster_data/src/main/kotlin/com/cleancode/pull_cluster_data/controller/route.kt
@@ -1,7 +1,5 @@
 package com.cleancode.pull_cluster_data.controller
 
-/*
-
- */
+// TODO [ALL] route 기능
 class route {
 }
