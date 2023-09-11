2a29f4d73bf0794d2c3f3587d883b1241694ada7
js3322 <js@cleancode.kr>
2023-09-11 21:00:20 +0900

    fix #code python naming rule

--- management_data_calc/main.py
+++ management_data_calc/main.py
@@ -1,8 +1,10 @@
 import pandas_example.Pandas001
 
+
 def main():
     data1 = pandas_example.Pandas001.Pandas001(20, 50)
     print(data1.get_data001())
+
 
 if __name__ == '__main__':
     print('hello world')
